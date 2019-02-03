fa670f00130fa3480f0f0ae638187d92ec965e61
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-02-03 21:14:50 +0200

    Add files via upload

---
+++ Programming Basics January 2019/Labs/While-Loop - Lab/07. Graduation pt.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace _07.Graduation_pt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double excluded = 0;
+            bool isExcluded = false;
+            int counter = 1;
+            double sum = 0;
+            while (counter <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade < 4)
+                {
+                    excluded++;
+                }
+                else if (grade >= 4)
+                {
+                    sum += grade;
+                    counter++;
+                }
+                if (excluded >= 2)
+                {
+                    isExcluded = true;
+                    break;
+                }
+            }
+                if (!isExcluded)
+                {
+                    double avg = sum / 12;
+                    Console.WriteLine($"{name} graduated. Average grade: {avg:f2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} has been excluded at {counter} grade");
+                }
+            
+        }
+    }
+}
---
+++ Programming Basics January 2019/Labs/While-Loop - Lab/08. Moving/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace _08.Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+            int freeSpace = width * lenght * hight;
+            int totalSpace = 0;
+            int spaceDiff = 0;
+            string command = Console.ReadLine();
+            while (command!="Done")
+            {
+                int space = int.Parse(command);
+                totalSpace += space;
+                spaceDiff = Math.Abs(totalSpace - freeSpace);
+                
+                if (totalSpace>=freeSpace)    
+                {
+                    Console.WriteLine($"No more free space! You need {spaceDiff} Cubic meters more.");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{spaceDiff} Cubic meters left.");
+        }
+    }
+}
