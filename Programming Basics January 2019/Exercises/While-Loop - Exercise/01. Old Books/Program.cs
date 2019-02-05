6c70a3801492916084f8702935ceb432a79c88b6
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-02-05 15:50:41 +0200

    added while loop Exercises

---
+++ Programming Basics January 2019/Exercises/While-Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace _01.Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Book = Console.ReadLine();
+            int count = 0;
+            int allowedBooks = int.Parse(Console.ReadLine());
+            while (count<allowedBooks)
+            {
+                string nextBook = Console.ReadLine();
+                if (nextBook==Book)
+                {
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    return;
+                }
+      
+                count++;  
+            }
+            Console.WriteLine("The book you search is not here!");
+            Console.WriteLine($"You checked {allowedBooks} books.");
+        }
+    }
+}
