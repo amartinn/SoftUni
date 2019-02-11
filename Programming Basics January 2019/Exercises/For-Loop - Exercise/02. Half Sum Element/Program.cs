179c32218f50a09eab1ef4626dd47c336f67e7d3
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-02-11 04:43:25 +0200

    added for-loop exercises

---
+++ Programming Basics January 2019/Exercises/For-Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace _02.Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num>max)
+                {
+                    max = num;
+                }
+            }
+            sum -= max;
+            if (sum==max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sum-max)}");
+            }
+        }
+    }
+}
---
+++ Programming Basics January 2019/Exercises/For-Loop - Exercise/06. Divide without remainder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace _06.Divide_without_remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0, p2 = 0, p3 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num%2==0)
+                {
+                    p1++;
+                }
+                if (num%3==0)
+                {
+                    p2++;
+                }
+                 if (num%4==0)
+                {
+                    p3++;
+                }
+            }   
+            p1 = p1 / n * 100;
+            p2 = p2 / n * 100;
+            p3 = p3 / n * 100;
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+        }
+    }
+}
