70bb4cbc60dc25a1506c92fe35b669c8ccdbc83b
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-02-16 13:39:24 +0200

    adding more exercises

---
+++ Programming Basics January 2019/Labs/Nested Loops - Lab/03. Even Powers of 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _03.Even_Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int current = 1;
+            for (int i = 0; i <=n; i+=2)
+            {
+                Console.WriteLine(current);
+                current *= 4;
+            }
+        }
+    }
+}
