285dd2b684eff123ef7abcbf0b0434a890a06032
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-02-16 14:17:20 +0200

    Add files via upload

---
+++ Programming Basics January 2019/Exercises/Nested Loops - Exercise/09. Password Generator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace _09.Password_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int first = 1; first < start; first++)
+            {
+                for (int second = 1; second < start; second++)
+                {
+                    for (char third = 'a'; third < end + 97; third++)
+                    {
+                        for (char forth = 'a'; forth < end + 97; forth++)
+                        {
+                            for (int fifth = 1; fifth <= start; fifth++)
+                            {
+                                if (fifth > first && fifth > second)
+                                {
+                                    Console.Write($"{first}{second}{third}{forth}{fifth} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
