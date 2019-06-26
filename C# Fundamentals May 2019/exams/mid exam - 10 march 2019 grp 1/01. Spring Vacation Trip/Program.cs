619e5b691a6bb345be05e920325a64901bde5e61
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-06-27 00:56:12 +0300

    Hello France Minor Mistake

---
+++ C# Fundamentals May 2019/exams/mid exam - 10 march 2019 grp 1/01. Spring Vacation Trip/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01._Spring_Vacation_Trip
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+            var days = int.Parse(Console.ReadLine());
+            var budget = double.Parse(Console.ReadLine());
+            var groupOfPeople = int.Parse(Console.ReadLine());
+            var fuelPerKM = double.Parse(Console.ReadLine());
+            var foodExpensesPerPerson = double.Parse(Console.ReadLine());
+            var hotelRoomPricePerPerson = double.Parse(Console.ReadLine());
+
+            if (groupOfPeople > 10)
+            {
+                hotelRoomPricePerPerson *= 0.75;
+            }
+            var currentExpenses = days * groupOfPeople * (foodExpensesPerPerson + hotelRoomPricePerPerson);
+
+            for (int i = 1; i <= days; i++)
+            {
+                var travelledDistance = double.Parse(Console.ReadLine());
+                currentExpenses += travelledDistance * fuelPerKM;
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    currentExpenses *= 1.4;
+                }
+                if (i % 7 == 0)
+                {
+                    currentExpenses -= currentExpenses / groupOfPeople;
+                }
+
+                if (currentExpenses > budget)
+                {
+                    Console.WriteLine($"Not enough money to continue the trip. You need {(currentExpenses - budget):f2}$ more.");
+                    return;
+                }
+            }
+            Console.WriteLine($"You have reached the destination. You have {(budget - currentExpenses):f2}$ budget left.");
+
+        }
+        }
+    }
---
+++ C# Fundamentals May 2019/exams/mid exam - 10 march 2019 grp 1/02. Hello, France/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Hello__France
+{
+    class Program
+    {
+        static double budget;
+        static void Main(string[] args)
+        {
+            var items = Console.ReadLine()
+                .Split("|")
+                .ToList();
+            var broughtItems = new List<double>();
+            var profitableItems = new List<double>();
+            var budget = double.Parse(Console.ReadLine());
+            Program.budget = budget;
+            for (int i = 0; i < items.Count; i++)
+            {
+                var tokens = items[i].Split("->");
+                var item = tokens[0];
+                var price = double.Parse(tokens[1]);
+                areItemsValid(item, price, broughtItems);
+            }
+
+            profitableItems = broughtItems.Select(x => x * 1.4).ToList();
+            var profitSum = profitableItems.Sum();
+            var sum = broughtItems.Sum();
+            if (profitableItems.Count != 0)
+            {
+                for (int i = 0; i < profitableItems.Count; i++)
+                {
+                    Console.Write($"{profitableItems[i]:f2} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Profit: {profitSum-sum:f2}");
+            if (Program.budget + profitSum >= 150)
+            {
+                Console.WriteLine("Hello, France!");
+            }
+            else
+            {
+                Console.WriteLine("Time to go.");
+            }
+           
+
+        }
+
+        private static void areItemsValid(string item, double price, List<double> broughtItems)
+        {
+            
+            if (item == "Clothes" && price <= 50 && budget-price>0)
+            {
+                budget -= price;
+                broughtItems.Add(price);
+            }
+            else if (item == "Shoes" && price <= 35 && budget - price > 0)
+            {
+                budget -= price;
+                broughtItems.Add(price);
+            }
+            else if (item == "Accessories" && price <= 20.5 && budget - price > 0)
+            {
+                budget -= price;
+                broughtItems.Add(price);
+            }
+        }
+    }
+}
