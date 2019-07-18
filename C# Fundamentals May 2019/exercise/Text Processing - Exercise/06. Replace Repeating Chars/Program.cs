5d0a9434c9f45cca0a15d0cd059e973913e2ce28
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-07-18 17:58:57 +0300

    added LettersChangeNumbers

--- C# Fundamentals May 2019/exercise/Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
+++ C# Fundamentals May 2019/exercise/Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace _06._Replace_Repeating_Chars
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+    
+                    
+               
+            }
+            
         }
     }
 }
---
+++ C# Fundamentals May 2019/exercise/Text Processing - Exercise/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _08._Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new char[] { ' ','\t'},StringSplitOptions.RemoveEmptyEntries);
+            var result = 0.0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentInput = input[i];
+                
+                    var firstLetter = currentInput[0];
+                    var lastLetter = currentInput[currentInput.Length - 1];
+                    var numberInBetween = double.Parse(currentInput.Substring(1, currentInput.Length-2));
+                if (char.IsUpper(firstLetter))
+                {
+                    numberInBetween /= firstLetter - 'A' + 1;
+                }
+                else
+                {
+                    numberInBetween *= firstLetter - 'a' + 1;
+                }
+                if (char.IsUpper(lastLetter))
+                {
+                    numberInBetween -= lastLetter - 'A' + 1;
+                }
+                else
+                {
+                    numberInBetween += lastLetter - 'a' + 1;
+                }
+                result += numberInBetween;
+            }
+            Console.WriteLine(result.ToString("f2"));
+
+        }
+    }
+}
