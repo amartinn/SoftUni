3a60de01ffbd058b503e575ff2710417bf6c0b8b
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-07-22 02:24:54 +0300

    fixed a bug

--- C# Fundamentals May 2019/exercise/Associative Arrays - More Exercise/02. Judge/Program.cs
+++ C# Fundamentals May 2019/exercise/Associative Arrays - More Exercise/02. Judge/Program.cs
@@ -10,12 +10,14 @@ namespace _02._Judge
             // https://judge.softuni.bg/Contests/Practice/Index/1302#1
             var contestUserAndPts = new Dictionary<string, Dictionary<string, int>>();
             var input = string.Empty;
+
             while ((input = Console.ReadLine())!="no more time")
             {
                 var line = input.Split(" -> ");
                 var user = line[0];
                 var contest = line[1];
-                var points = int.Parse(line[2]);            
+                var points = int.Parse(line[2]); 
+                
                 if (!contestUserAndPts.ContainsKey(contest))
                 {
                     contestUserAndPts[contest] = new Dictionary<string, int>();
@@ -23,36 +25,35 @@ namespace _02._Judge
                    
                 }
                 else if (contestUserAndPts.ContainsKey(contest) && !contestUserAndPts[contest].ContainsKey(user))
-                {
-                 
                     contestUserAndPts[contest].Add(user, points);
-                    
-                }
+
                  if (contestUserAndPts.ContainsKey(contest) && contestUserAndPts[contest].ContainsKey(user))
-                {
+                 {
                     if (contestUserAndPts[contest][user]<points)
                     {
-                        contestUserAndPts[contest][user] = points;
-                           
-                    }
-                    
-                }   
+                        contestUserAndPts[contest][user] = points;  
+                    } 
+                 }
             }
              var count = 1;
             foreach (var contest in contestUserAndPts)
-        {
-               
-            Console.WriteLine($"{contest.Key}: {contest.Value.Keys.Count} participants");
-            foreach (var users in contest.Value.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{contest.Key}: {contest.Value.Keys.Count} participants");
+
+            foreach (var users in contest
+                    .Value
+                    .OrderByDescending(x=>x.Value)
+                    .ThenBy(x=>x.Key))
             {
                 
                 Console.WriteLine($"{count}. {users.Key} <::> {users.Value}");
                 count++;
             }
-        }
+                count = 1;
+            }
             Console.WriteLine("Individual standings:");
             count = 1;
-            // TODO
+            //TODO list Individuals
         }
     }
 }
