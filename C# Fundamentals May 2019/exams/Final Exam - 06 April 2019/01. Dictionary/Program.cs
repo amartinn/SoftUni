a2cd3332efece23474b8193f8b60cc519d3dd34a
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-07-27 00:31:50 +0300

    Dictionary 100/100

--- C# Fundamentals May 2019/exams/Final Exam - 06 April 2019/01. Dictionary/Program.cs
+++ C# Fundamentals May 2019/exams/Final Exam - 06 April 2019/01. Dictionary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Dictionary
 {
@@ -6,7 +8,57 @@ namespace _01._Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var termAndDescription = new Dictionary<string, List<string>>();
+
+            var firstLine = Console.ReadLine()
+                .Split(" | "
+                ,StringSplitOptions.RemoveEmptyEntries);
+
+            var secondLine = Console.ReadLine()
+                .Split(" | "
+                , StringSplitOptions.RemoveEmptyEntries);
+
+            var thirdLine = Console.ReadLine();
+
+            var words = new List<string>();
+
+            foreach (var sentance in firstLine)
+            {
+                var line  = sentance.Split(": ");
+                var term = line[0];
+                var description = line[1];
+                if (!termAndDescription.ContainsKey(term))
+                {
+                    termAndDescription[term] = new List<string>();
+                }
+                termAndDescription[term].Add(description);
+            }
+            foreach (var word in secondLine)
+            {
+                words.Add(word);
+            }
+            if (thirdLine=="List")
+            {
+                termAndDescription = termAndDescription
+                    .OrderBy(x => x.Key)
+                    .ToDictionary(k => k.Key, v => v.Value);
+                Console.WriteLine(string.Join(" ",termAndDescription.Keys));
+            }
+            else
+            {
+                words = words.Distinct().ToList();
+                foreach (var term in termAndDescription.Distinct().OrderBy(x=>x.Key))
+                {
+                    if (words.Contains(term.Key))
+                    {
+                        Console.WriteLine(term.Key);
+                        foreach (var description in term.Value.OrderByDescending(x => x.Length))
+                        {
+                            Console.WriteLine($"-{description}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
