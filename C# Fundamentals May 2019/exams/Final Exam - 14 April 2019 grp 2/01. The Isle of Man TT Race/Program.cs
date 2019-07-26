ded997a62e27495b13d02d94ad472ab3f5510023
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-07-26 18:42:34 +0300

    added first task

---
+++ C# Fundamentals May 2019/exams/Final Exam - 14 April 2019 grp 2/01. The Isle of Man TT Race/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._The_Isle_of_Man_TT_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var geoHashCode = new StringBuilder();
+            var nameOfRacer = string.Empty;
+            var isValid = false;
+            while (!isValid)
+            {
+                var input = Console.ReadLine();
+                var lenghtPattern = @"=(\d+)!!";
+                var lenghtMatch = Regex.Match(input, lenghtPattern);
+                if (lenghtMatch.Success)
+                {
+                    var pattern = $@"([$%*&#])(\w+)(\1)=(\d+)!!(.{{{lenghtMatch.Groups[1].Value}}})$";
+                    var match = Regex.Match(input, pattern);
+                    if (match.Success)
+                    {
+                        nameOfRacer = match.Groups[2].Value;
+                        var coordinates = match.Groups[5].Value;
+                        var lenght = int.Parse(lenghtMatch.Groups[1].Value);
+                        foreach (var cord in coordinates)
+                        {
+                            geoHashCode.Append((char)(cord+lenght));
+                        }
+                        isValid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Nothing found!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Nothing found!");
+                }
+
+
+            }
+            Console.WriteLine($"Coordinates found! {nameOfRacer} -> {geoHashCode.ToString()}");
+            }
+    }
+}
