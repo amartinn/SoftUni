71466ea77bc996a63063417f55a9b39c67a8f22b
Martin Angelov <47275385+amartinn@users.noreply.github.com>
2019-07-27 00:39:44 +0300

    Deciphering 100/100

---
+++ C# Fundamentals May 2019/exams/Final Exam - 06 April 2019/02. Deciphering/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encryptedString = Console.ReadLine();
+            var substrings = Console.ReadLine().Split();
+            var decryptedString = string.Empty;
+            var pattern = @"^[{}|#d-z]+$";
+            var match = Regex.Match(encryptedString, pattern);
+            if (match.Success)
+            {
+                foreach (var @char in encryptedString)
+                {
+                    decryptedString += (char)(@char - 3);
+                }
+                decryptedString = decryptedString.Replace(substrings[0], substrings[1]);
+                Console.WriteLine(decryptedString);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+            
+        }
+    }
+}
