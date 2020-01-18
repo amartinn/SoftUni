7719e0c8b03e8cb53fc3101dfb6a8e18d4790fb3
Martin Angelov <martoy123691@gmail.com>
2020-01-18 22:03:42 +0200

    refactoring

--- JS Core January 2020/JS Advanced/exercises/Objects/HeroicInventory.js
+++ JS Core January 2020/JS Advanced/exercises/Objects/HeroicInventory.js
@@ -18,6 +18,3 @@ function solution(input){
     }
     console.log(JSON.stringify(data));
 }
-solution(['Isacc / 25 / Apple, GravityGun',
-'Derek / 12 / BarrelVest, DestructionSword',
-'Hes / 1 / Desolator, Sentinel, Antara'])
