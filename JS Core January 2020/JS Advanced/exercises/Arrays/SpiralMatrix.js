c6aea4dff912848c7f8b48c9f2dc976b672cc895
Martin Angelov <martoy123691@gmail.com>
2020-01-19 16:09:23 +0200

    added spiral matrix

---
+++ JS Core January 2020/JS Advanced/exercises/Arrays/SpiralMatrix.js
@@ -0,0 +1,42 @@
+function spiralMatrix(rows,cols) {
+    const matrix = new Array(rows)
+    .fill()
+    .map(() => new Array(cols).fill(''));
+
+    let counter = 1;
+    let startCol = 0;
+    let endCol = cols - 1;
+    let startRow = 0;
+    let endRow = rows - 1;
+    while (startCol <= endCol && startRow <= endRow) {
+        for (let i = startCol; i <= endCol; i++) {
+            matrix[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+        for (let j = startRow; j <= endRow; j++) {
+            matrix[j][endCol] = counter;
+            counter++;
+        }
+
+        endCol--;
+
+        for (let i = endCol; i >= startCol; i--) {
+            matrix[endRow][i] = counter;
+            counter++;
+        }
+
+        endRow--;
+        for (let i = endRow; i >= startRow; i--) {
+            matrix[i][startCol] = counter;
+            counter++;
+        }
+
+        startCol++;
+
+    }
+    matrix.forEach(row => {
+        console.log(row.join(' '));
+    });
+}
+spiralMatrix(5,5);
