d174f481ba93d72a84f7a3126f510820c9475f7f
Martin Angelov <martoy123691@gmail.com>
2020-01-19 16:42:10 +0200

    fixed an issue

--- JS Core January 2020/JS Advanced/exercises/Arrays/ExtractArray.js
+++ JS Core January 2020/JS Advanced/exercises/Arrays/ExtractArray.js
@@ -3,12 +3,12 @@ function extract(input) {
     const current = input[0];
     output.push(current);
     let biggest = current;
-    input.reduce(function (NaN, next) {
-        if (next >= biggest) {
-            output.push(next);
-            biggest = next;
-        }
-
-    });
+   input.forEach(function(curr,index,source){
+       let next = source[index+1];
+       if(next>=biggest){
+           output.push(next);
+           biggest=next;
+       }
+   });
     console.log(output.join('\n'));
 }
