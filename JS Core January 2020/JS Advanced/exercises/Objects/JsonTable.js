2369f43b27f90a2d4ca09eca99faf2f3ae3c2feb
Martin Angelov <martoy123691@gmail.com>
2020-01-26 18:52:20 +0200

    refactoring

--- JS Core January 2020/JS Advanced/exercises/Objects/JsonTable.js
+++ JS Core January 2020/JS Advanced/exercises/Objects/JsonTable.js
@@ -1,16 +1,25 @@
-function table(input){
-console.log(`<table>`);
-    let data = [];
-input.forEach(element => {
-    console.log(`\t<tr>`);
-    let json = JSON.parse(element);
-    let name = json.name;
-    let position = json.position;
-    let salary = json.salary;
-    console.log(`\t\t<td>${name}</td>`);
-    console.log(`\t\t<td>${position}</td>`);
-    console.log(`\t\t<td>${salary}</td>`);
-    console.log(`\t</tr>`);
-});
-console.log(`</table>`);
+function table(data) {
+
+    const parsedData = data.map(x => JSON.parse(x));
+
+    const table = content => `<table>\n${content}\n</table>`;
+    const tableRow = content => `\t<tr>\n${content}\t</tr>\n`;
+    const tableData = content => `\t\t<td>${content}</td>\n`;
+
+    let output = parsedData.reduce((accRow, row) => {
+
+        let tdForPeson = Object.values(row).reduce((tdAcc, td) => {
+            return tdAcc + tableData(td);
+
+        }, '');
+     
+        return accRow + tableRow(tdForPeson);
+    },'');
+
+    output = table(output);
+    
+    console.log(output);
 }
+table(['{"name":"Pesho","position":"Promenliva","salary":100000}',
+'{"name":"Teo","position":"Lecturer","salary":1000}',
+'{"name":"Georgi","position":"Lecturer","salary":1000}']);
