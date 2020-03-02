e3567850dfe9c51e62fa7954b45cb8fdfe9c7b51
Martin Angelov <martoy123691@gmail.com>
2020-03-02 19:07:48 +0200

    removed input

--- JS Core January 2020/JS Applications/This/company.js
+++ JS Core January 2020/JS Applications/This/company.js
@@ -53,12 +53,3 @@ class Company {
 		return temp.sort((a, b) => a.average > b.average)[0];
 	}
 }
-let c = new Company();
-c.addEmployee('Stanimir', 2000, 'engineer', 'Human resources');
-c.addEmployee('Pesho', 1500, 'electrical engineer', 'Construction');
-c.addEmployee('Slavi', 500, 'dyer', 'Construction');
-c.addEmployee('Stan', 2000, 'architect', 'Construction');
-c.addEmployee('Stanimir', 1200, 'digital marketing manager', 'Marketing');
-c.addEmployee('Pesho', 1000, 'graphical designer', 'Marketing');
-c.addEmployee('Gosho', 1350, 'HR', 'Human resources');
-console.log(c.bestDepartment());
