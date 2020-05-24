bd84605b4d037d743fc0a064914629c38b381e81
Martin Angelov <martoy123691@gmail.com>
2020-05-24 19:03:21 +0300

    added table relations

---
+++ C# DB May 2020/MS SQL/Exercises/Table Relations/Table Relations.sql
@@ -0,0 +1,167 @@
+/* 1 */
+CREATE DATABASE [Table Relations]
+USE [Table Relations]
+
+CREATE TABLE Passports (
+PassportID INT PRIMARY KEY NOT NULL,
+PassportNumber NVARCHAR(8) NOT NULL)
+
+CREATE TABLE Persons (
+PersonID INT PRIMARY KEY IDENTITY,
+FirstName NVARCHAR(15) NOT NULL,
+Salary DECIMAL(7,2) NOT NULL,
+PassportID INT FOREIGN KEY REFERENCES Passports(PassportID)
+)
+INSERT INTO Passports VALUES
+(101,'N34FG21B'),
+(102,'K65LO4R7'),
+(103,'ZE657QP2')
+
+INSERT INTO Persons VALUES
+('Roberto',43300.00,102),
+('Tom',56100.00,103),
+('Yana',60200.00,101)
+
+/* 2 */
+CREATE TABLE Manufacturers(
+ManufacturerID INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(10) NOT NULL,
+EstablishedOn DATE NOT NULL)
+
+CREATE TABLE Models (
+ModelID INT PRIMARY KEY NOT NULL,
+[Name] NVARCHAR(10) NOT NULL,
+ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID),
+)
+
+INSERT INTO Manufacturers VALUES
+('BMW','07/03/1916'),
+('Tesla','01/01/2003'),
+('Lada','01/05/1966')
+
+
+INSERT INTO Models VALUES 
+(101,'X1',1),
+(102,'i6',1),
+(103,'Model S',2),
+(104,'Model X',2),
+(105,'Model 3',2),
+(106,'Nova',3)
+
+/* 3 */
+CREATE TABLE Students (
+StudentID INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(50) NOT NULL)
+
+CREATE TABLE Exams(
+ExamID INT PRIMARY KEY NOT NULL IDENTITY(101,1),
+[Name] NVARCHAR(50) NOT NULL)
+
+
+CREATE TABLE StudentsExams (
+StudentID INT NOT NULL,
+ExamID INT NOT NULL,
+PRIMARY KEY(StudentID,ExamID))
+
+INSERT INTO Students VALUES
+('Mila  '),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams VALUES
+(1,101),
+(1,102),
+(2,101),
+(3,103),
+(2,102),
+(2,103)
+
+/* 4 */
+
+CREATE TABLE Teachers(
+TeacherID INT PRIMARY KEY IDENTITY(101,1),
+[Name] NVARCHAR(20) NOT NULL,
+ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID))
+
+	
+INSERT INTO Teachers VALUES
+('John',NULL),
+('Maya',106),
+('Silvia',106),
+('Ted',105),
+('Mark',101),
+('Greta',101)
+
+/* 5 */
+
+CREATE TABLE Cities(
+CityID INT PRIMARY KEY  NOT NULL ,
+[Name] NVARCHAR(50) NOT NULL )
+
+CREATE TABLE Customers(
+CustomerID INT PRIMARY KEY  NOT NULL ,
+[Name] VARCHAR(50)  NOT NULL ,
+Birthday DATE,
+CityID INT FOREIGN KEY REFERENCES Cities(CityID)  NOT NULL )
+
+CREATE TABLE Orders(
+OrderID INT PRIMARY KEY  NOT NULL ,
+CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID)  NOT NULL )
+
+CREATE TABLE ItemTypes(
+ItemTypeID INT PRIMARY KEY  NOT NULL ,
+[Name] VARCHAR(50)  NOT NULL )
+
+CREATE TABLE Items(
+ItemID INT PRIMARY KEY  NOT NULL ,
+[Name] VARCHAR(50)  NOT NULL ,
+ItemTypeID INT FOREIGN KEY REFERENCES ItemTypes(ItemTypeID)  NOT NULL )
+
+CREATE TABLE OrderItems(
+OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
+ItemID INT FOREIGN KEY REFERENCES Items(ItemID),
+PRIMARY KEY(OrderID,ItemID))
+
+/* 6 */
+
+CREATE TABLE Majors (
+	MajorID INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students (
+	StudentID INT PRIMARY KEY NOT NULL,
+	StudentNumber VARCHAR(15) NOT NULL,
+	StudentName VARCHAR(50) NOT NULL,
+	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments (
+	PaymentID INT PRIMARY KEY NOT NULL,
+	PaymentDate DATETIME NOT NULL,
+	PaymentAmount DECIMAL(6, 2) NOT NULL,
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects (
+	SubjectID INT PRIMARY KEY NOT NULL,
+	SubjectName VARCHAR(35) NOT NULL
+)
+
+CREATE TABLE Agenda (
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID)
+	PRIMARY KEY (StudentID, SubjectID)
+)
+
+/* 9 */
+USE Geography
+SELECT M.MountainRange, P.PeakName, P.Elevation FROM Mountains AS M
+JOIN Peaks AS P ON P.MountainId = M.Id
+WHERE M.MountainRange = 'Rila'
+ORDER BY P.Elevation DESC
